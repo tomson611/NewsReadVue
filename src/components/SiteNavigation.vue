bcefe4810d812e217ef0d8c871225493cf5ceea0
Tomasz <twesolowski61@gmail.com>
2024-06-16 09:13:17 +0200

    Add navigation

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,33 +1,51 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">NewsRead</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <RouterLink to="/" class="nav-link">Home</RouterLink>
-            <RouterLink to="#" class="nav-link">Read</RouterLink>
-            <RouterLink to="#" class="nav-link">Search</RouterLink>
-            <RouterLink to="#" class="nav-link">Sources</RouterLink>
-          </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <RouterLink to="/" class="nav-link">NewsRead</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <RouterLink to="/read" class="nav-link">
+            <template v-slot="{ isActive }">
+              <span :class="{ active: isActive }">Read</span>
+            </template>
+          </RouterLink>
+          <RouterLink to="/search" class="nav-link">
+            <template v-slot="{ isActive }">
+              <span :class="{ active: isActive }">Search</span>
+            </template>
+          </RouterLink>
+          <RouterLink to="/sources" class="nav-link">
+            <template v-slot="{ isActive }">
+              <span :class="{ active: isActive }">Sources</span>
+            </template>
+          </RouterLink>
         </div>
       </div>
-    </nav>
-    <RouterView />
-  </header>
+    </div>
+  </nav>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.active {
+  font-weight: bold;
+  font-size: 17px;
+}
+
+.nav-link {
+  font-size: 18px;
+}
+</style>
